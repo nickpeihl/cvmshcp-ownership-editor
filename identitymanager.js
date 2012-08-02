6eb76641d2e1f2eed77ff31cab6beb2f1649be8a
Nick Peihl <npeihl@cvag.org>
2012-08-02 11:31:09 -0700

    Added Datagrid and Find task for APNs or Ownership.

--- identitymanager.js
+++ identitymanager.js
@@ -1,11 +1,18 @@
 dojo.require("dijit.layout.BorderContainer");
 dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.form.Button");
 dojo.require("esri.map");
 dojo.require("esri.layers.FeatureLayer");
 dojo.require("esri.dijit.AttributeInspector-all");
 dojo.require("esri.IdentityManager");
+dojo.require("esri.tasks.find");
+dojo.require("dojox.grid.DataGrid");
+dojo.require("dojo.date.ItemFileReadStore");
 
+
+var findTask, findParams;
 var map, cred = "esri_jsapi_id_manager_data"; // cookie/local storage name
+var grid, store;
 
 
 function init() {
@@ -14,6 +21,8 @@ function init() {
     dojo.addOnUnload(storeCredentials);
     // look for credentials in local storage
     loadCredentials();
+    
+    dojo.connect(grid, "onRowClick", onRowClickHandler);
 
     var startExtent = new esri.geometry.Extent(-116.7055,33.9913,-115.7981,33.5033, new esri.SpatialReference({wkid:4326}));
 
@@ -46,6 +55,15 @@ function init() {
 		 });
 
     map.addLayers([ownershipFL]);
+
+    findTask = new esri.tasks.FindTask("https://cvag01.mojavedata.gov/ArcGIS/rest/services/Ownership_Editor/FeatureServer/0");
+    
+    // Create the find parameters
+    findParams = new esri.tasks.FindParameters();
+    findParams.returnGeometry = true;
+    findParams.layerIds = [0];
+    findParams.searchFields = ["APN", "OWNER"];
+    findParams.outSpatialReference = map.spatialReference;
 
     function loadCredentials() {
 	var idJson, idObject;
@@ -127,7 +145,7 @@ function initSelectToolbar(results) {
 			   {'fieldName': 'APN', 'isEditable':false, 'label':'APN:'},
 			   {'fieldName': 'OWNER', 'isEditable':true, 'tooltip': 'The Ownership of this Parcel', 'label':'Owner:'},
 			   {'fieldName': 'CONS_STATUS', 'isEditable':true, 'tooltip': 'Is this Parcel Conserved?','label':'Conserved?:'},
-			   {'fieldName': 'POST_MOU', 'isEditable':true, 'tooltip': 'Was this Parcel Conserved after 1996?', 'label':'Post MOU Conserved?:'},
+			   {'fieldName': 'POST_MOU', 'isEditable':true, 'tooltip': 'Was this Parcel Conserved after 1996?', 'label':'Acquired Since 1996?:'},
 			   {'fieldName': 'ACQ_DATE', 'isEditable':true, 'tooltip': 'Date Parcel was Acquired for Conservation', 'label':'Acquisition Date:'},
 			   {'fieldName': 'CREDIT_STATE', 'isEditable':true, 'tooltip': 'Federal/State Funding (Percent)', 'label':'Federal/State Funding (Percent):'},
 			   {'fieldName': 'CREDIT_PERMITTEE', 'isEditable':true, 'tooltip': 'Local Permittee Funding (Percent)', 'label':'Local Permittee Funding (Percent):'},
@@ -144,7 +162,7 @@ function initSelectToolbar(results) {
     
     //add a save button next to the delete button
     var saveButton = new dijit.form.Button({label:"Save","class":"saveButton"});
-    dojo.place(saveButton.domNode, attInspector.deleteBtn.domNode, "after");
+    dojo.place(saveButton.domNode, "after");
 
     dojo.connect(saveButton,"onClick",function(){
 		     updateFeature.getLayer().applyEdits(null, [updateFeature],null);
@@ -160,16 +178,61 @@ function initSelectToolbar(results) {
 		 console.log("Next " + updateFeature.attributes.objectid);
 		 });
 
-    dojo.connect(attInspector, "onDelete", function(feature){
-		 feature.getLayer().applyEdits(null,null,[feature]);
-		 map.infoWindow.hide();
-		 });
-
     map.infoWindow.setContent(attInspector.domNode);
-    map.infoWindow.resize(325,210);
+    map.infoWindow.resize(450,300);
     
     
 }
+
+function doFind() {
+    //Set the search text to the value in the box
+    findParams.searchText = dojo.byId("searchBox").value;
+    findTask.execute(findParams,showResults);
+    }
+
+function showResults(results) {
+    //This function works with an array of FindResult that the task returns
+    map.graphics.clear();
+    var symbol = new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([98,194,204]),2), new dojo.Color([98,194,204,0.5]));
+    
+    //create array of attributes
+    var items = dojo.map(results,function(result){
+			     var graphic = result.feature;
+			 graphic.setSymbol(symbol);
+			 map.graphics.add(graphic);
+			 return result.feature.attributes;
+			 });
+    
+    var data = {
+	identifier: "APN",
+	label: "APN",
+	items: items
+    };
+
+    store = new dojo.data.ItemFileReadStore({ data:data });
+    var grid = dijit.byID('grid');
+    grid.setStore(store);
+    
+    //Zoom back to the initial map extent
+    map.setExtent(startExtent);
+}
+
+function onRowClickHandler(evt){
+    var clickedParcelID = grid.getItem(evt.rowIndex).APN;
+    var selectedParcelID;
+
+    dojo.forEach(map.graphics.graphics, function(graphic){
+		 if((graphic.attributes) && graphic.attributes.APN === clickedParcelID){
+		     selectedParcelID = graphic;
+		     return;
+		 }
+		
+    });
+    var parcelExtent = selectedParcelID.geometry.getExtent();
+    map.setExtent(parcelIDExtent);
+}
+
+
 
 
 dojo.addOnLoad(init);
